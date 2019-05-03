c97f176528de5e48acf05dd561a573ca0ed083b2
liamf90 <42581285+liamf90@users.noreply.github.com>
2019-05-03 14:50:51 +1000

    first

---
+++ .externalNativeBuild/ndkBuild/debug/obj/local/arm64-v8a/objs/cpufeatures/cpu-features.o.d
@@ -0,0 +1,194 @@
+C:\Users\Liam\StudioProjects\SnapBattle2\snapBattle2\.externalNativeBuild\ndkBuild\debug\obj/local/arm64-v8a/objs/cpufeatures/cpu-features.o: \
+ C:/Users/Liam/Downloads/android-ndk-r10e/sources//android/cpufeatures/cpu-features.c \
+ C:/Users/Liam/Downloads/android-ndk-r10e/sources//android/cpufeatures/cpu-features.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/android/api-level.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/stdint.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/machine/wchar_limits.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/dlfcn.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/errno.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/errno.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/errno.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/errno.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/errno-base.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/fcntl.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/types.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/types.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/types.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/types.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/int-ll64.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/bitsperlong.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/bitsperlong.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/posix_types.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/stddef.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/compiler.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/posix_types.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/posix_types.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/sysmacros.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/fadvise.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/fcntl.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/fcntl.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/fcntl.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/uio.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/unistd.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/select.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/time.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/time.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/signal.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/limits.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/limits.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/limits.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/syslimits.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/string.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/malloc.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/xlocale.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/sigcontext.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/signal.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/signal.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/signal.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/signal-defs.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/siginfo.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/siginfo.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/ucontext.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/user.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/sysconf.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/pathconf.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/pthread.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/time.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sched.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/sched.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/stdio.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/stdlib.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/alloca.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/strings.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/memory.h \
+ C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/system_properties.h
+
+C:/Users/Liam/Downloads/android-ndk-r10e/sources//android/cpufeatures/cpu-features.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/android/api-level.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/stdint.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/machine/wchar_limits.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/dlfcn.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/errno.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/errno.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/errno.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/errno.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/errno-base.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/fcntl.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/types.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/types.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/types.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/types.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/int-ll64.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/bitsperlong.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/bitsperlong.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/posix_types.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/stddef.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/compiler.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/posix_types.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/posix_types.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/sysmacros.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/fadvise.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/fcntl.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/fcntl.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/fcntl.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/uio.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/unistd.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/select.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/time.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/time.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/signal.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/limits.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/limits.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/limits.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/syslimits.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/string.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/malloc.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/xlocale.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/sigcontext.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/signal.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/signal.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/signal.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/signal-defs.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm/siginfo.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/asm-generic/siginfo.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/ucontext.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/user.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/sysconf.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/pathconf.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/pthread.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/time.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sched.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/linux/sched.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/stdio.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/stdlib.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/alloca.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/strings.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/memory.h:
+
+C:/Users/Liam/Downloads/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/system_properties.h:
